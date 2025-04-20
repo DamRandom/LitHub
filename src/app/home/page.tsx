fa6d19ae92285316c8bd360d4ecb1f924c05d9a1
DamRandom <britohdamian@gmail.com>
2025-04-20 00:34:56 -0400

    Update styles with new color scheme for background, text, and button

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,10 +4,8 @@ import ContinueReading from '@/components/ContinueReading'
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gray-100">
-      <div className="p-6">
-        <ContinueReading />
-      </div>
+    <div className="min-h-screen w-full bg-gray-100 m-0 p-0">
+      <ContinueReading />
     </div>
   )
 }
